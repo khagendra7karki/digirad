1f75bf8f0e0812a0f14d0497e95b0e4b2bf560e4
khagendra7karki <077bct036.khagendra@pcampus.edu.np>
2023-06-24 14:49:12 +0545

    audio player added

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-console.log('Hello World')
-//jquery
-$('h1').hide()
+var audioElement = $('#audio-player');
+$( document ).ready( () =>{
+    var audioTracks;
+    $.ajax({
+        url: 'audio.xml',
+        dataType : 'xml',
+        success: ( xml) =>{
+            audioTracks = $(xml).find( 'track')
+            loadTrack( audioTracks)
+        },
+
+        error: ( err ) =>{
+            console.log( 'Errror loading XML file' + err )
+
+        }
+
+    })
+})
+
+function loadTrack( audioTracks ){
+    var url = $( audioTracks ).find('url').text();
+    // var title = $( audioTracks ).find('title').text();   
+    audioElement.attr('src', url);
+    audioElement.load() 
+}
